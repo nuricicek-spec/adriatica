960a22ecc162a5a55f580f1a8ed7d3cbb256990a
Adriatica DOO <info@adriaticadoo.me>
2026-04-13 00:20:31 +0400

    açıklama

--- client/src/config/trustMetrics.ts
+++ client/src/config/trustMetrics.ts
@@ -1,6 +1,6 @@
 export const TRUST_METRICS = {
   yearsExperience: 20,
-  field: "marine engineering & compliance",
+  field: "Team Experience in Marine Engineering",
   vesselsSupported: 42,
   pscDetentions: 0,
   countries: 8,
